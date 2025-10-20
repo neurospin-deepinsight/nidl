816b775e17c26a3e6c0164deddd5c8cd518828f2
AGrigis <antoine.grigis@cea.fr>
2025-10-20 13:44:43 +0200

    nidl/info: increment version.

--- nidl/info.py
+++ nidl/info.py
@@ -10,7 +10,7 @@
 # Module current version
 version_major = 0
 version_minor = 0
-version_micro = 0
+version_micro = 1
 
 # Expected by setup.py: string of form "X.Y.Z"
 __version__ = f"{version_major}.{version_minor}.{version_micro}.dev"
