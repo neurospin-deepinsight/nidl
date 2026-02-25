95852aaf0ae0c9791189f1973d43f8e96758c52b
Raphael VOCK <rv275890@is157086.intra.cea.fr>
2026-02-25 11:51:50 +0100

    fixed metrics __init__.py

--- nidl/metrics/__init__.py
+++ nidl/metrics/__init__.py
@@ -1,2 +1,2 @@
 from .regression import pearson_r, regression_report
-from .ssl import alignment_score, contrastive_accuracy_score, uniformity_score
+from .ssl import alignment_score, contrastive_accuracy_score, uniformity_score, procrustes_similarity, procrustes_r2, kruskal_similarity
